6128958246e4bcf0214b05b7384b36c6b995c00d
Mahbubul Hasan <mahbubul161@gmail.com>
2026-09-04 13:55:36 +0600

    Implementation for 2942. Find Words Containing Character

---
+++ leetcode/2942. Find Words Containing Character/index.ts
@@ -0,0 +1,14 @@
+function findWordsContaining(words: string[], x: string): number[] {
+  let result: number[] = [];
+
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].includes(x)) result.push(i);
+  }
+
+  return result;
+}
+
+console.log(
+  "🚀 ~ findWordsContaining:",
+  findWordsContaining(["leet", "code"], "e"),
+);
