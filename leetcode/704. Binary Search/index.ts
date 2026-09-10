65f93094c5eb045338297efbb1e6ce5d61fb3b2e
Mahbubul Hasan <mahbubul161@gmail.com>
2026-09-10 08:24:18 +0600

    Add recursive binary search implementation with console tests

--- leetcode/704. Binary Search/index.ts
+++ leetcode/704. Binary Search/index.ts
@@ -12,6 +12,30 @@ function search(nums: number[], target: number): number {
   }
   return -1;
 }
+// console.log("🚀 ~ search:", search([-1, 0, 3, 5, 9, 12], 9));
+// console.log("🚀 ~ search:", search([-1, 0, 3, 5, 9, 12], 2));
 
-console.log("🚀 ~ search:", search([-1, 0, 3, 5, 9, 12], 9));
-console.log("🚀 ~ search:", search([-1, 0, 3, 5, 9, 12], 2));
+// Recursive BinarySearch
+function recursiveSearch(nums: number[], target: number): number {
+  return recursiveBinarySearch(nums, target, 0, nums.length - 1);
+}
+
+function recursiveBinarySearch(
+  nums: number[],
+  target: number,
+  start: number,
+  end: number,
+): number {
+  if (start > end) return -1;
+
+  let mid = Math.floor((start + end) / 2);
+
+  if (nums[mid] == target) return mid;
+
+  return nums[mid] < target
+    ? recursiveBinarySearch(nums, target, mid + 1, end)
+    : recursiveBinarySearch(nums, target, start, mid - 1);
+}
+
+console.log("🚀 ~ search:", recursiveSearch([-1, 0, 3, 5, 9, 12], 9));
+console.log("🚀 ~ search:", recursiveSearch([-1, 0, 3, 5, 9, 12], 2));
