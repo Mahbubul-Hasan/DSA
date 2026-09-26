7476b6c91003034b9d0f10ab3d4413b7b51cbed3
Mahbubul Hasan <mahbubul161@gmail.com>
2026-09-26 19:35:11 +0600

    Refactor triangle function to implement selection sort algorithm

--- test/index.ts
+++ test/index.ts
@@ -1,15 +1,22 @@
-function triangle(n: number) {
-  function helper(row: number, col: number) {
-    if (row == n) return;
+function selectionSort(arr: number[]): number[] {
+  const n = arr.length;
+  function helper(row: number, col: number): number[] {
+    if (row == n) return arr;
 
-    if (col <= n - 1 - row) {
-      process.stdout.write("* ");
+    if (col <= n - 1) {
+      if (arr[row] > arr[col]) {
+        [arr[row], arr[col]] = [arr[col], arr[row]];
+      }
       helper(row, ++col);
     } else {
-      console.log();
-      helper(++row, 0);
+      row = row + 1;
+      col = row;
+      helper(row, col);
     }
+
+    return arr;
   }
-  helper(0, 0);
+  return helper(0, 0);
 }
-triangle(4);
+
+console.log("🚀 ~ selectionSort:", selectionSort([4, 3, 2, 1]));
