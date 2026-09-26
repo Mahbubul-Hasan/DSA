c9ce935b5b7f722ab771a80d03a5a4c20953ee0f
Mahbubul Hasan <mahbubul161@gmail.com>
2026-09-26 19:40:00 +0600

    Rename selectionSort to bubbleSort and update sorting logic

--- test/index.ts
+++ test/index.ts
@@ -1,22 +1,18 @@
-function selectionSort(arr: number[]): number[] {
+function bubbleSort(arr: number[]): number[] {
   const n = arr.length;
   function helper(row: number, col: number): number[] {
     if (row == n) return arr;
 
-    if (col <= n - 1) {
-      if (arr[row] > arr[col]) {
-        [arr[row], arr[col]] = [arr[col], arr[row]];
+    if (col < n - 1) {
+      if (arr[col] > arr[col + 1]) {
+        [arr[col], arr[col + 1]] = [arr[col + 1], arr[col]];
       }
       helper(row, ++col);
-    } else {
-      row = row + 1;
-      col = row;
-      helper(row, col);
-    }
+    } else helper(++row, 0);
 
     return arr;
   }
   return helper(0, 0);
 }
 
-console.log("🚀 ~ selectionSort:", selectionSort([4, 3, 2, 1]));
+console.log("🚀 ~ bubbleSort:", bubbleSort([4, 3, 2, 1]));
